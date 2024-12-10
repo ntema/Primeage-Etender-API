50d4274fa5298c888471d1f30ce7c05c8d093ab9
unknown <emmanuelntekim000@gmail.com>
2024-12-10 22:33:17 +0100

    ...

--- src/server.js
+++ src/server.js
@@ -42,7 +42,7 @@ app.use((req, res, next) => {
 
 app.use((error, req, res, next) => {
   if (error instanceof mongoose.CastError) {
-    res.status(400).json({ message: "Invalid Mongo ID format" });
+    res.status(400).json({ message: "Invalid ID format" });
   }
 });
 
