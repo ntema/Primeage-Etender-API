35180eeadfc1e8c237ec1d07387742a315a0d23c
unknown <emmanuelntekim000@gmail.com>
2024-12-12 16:35:38 +0100

    ...

--- src/routes/uploadRoutes.js
+++ src/routes/uploadRoutes.js
@@ -19,21 +19,21 @@ uploadRouter.post('/uploadfile',upload.single("image"),(req, res) => {
     });
 })
 
-uploadRouter.post("/uploadvideo", upload.videoUpload.single("video"), (req, res) => {
-  cloudinary.uploader.upload(req.file.path, (err, result) => {
-    if (err) {
-      console.log(err);
-      return res.status(400).json({
-        success: false,
-        message: "Error",
-      });
-    }
-    res.status(200).json({
-      success: true,
-      message: "Uploaded!",
-      data: result,
-    });
-  });
-});
+// uploadRouter.post("/uploadvideo", upload.videoUpload.single("video"), (req, res) => {
+//   cloudinary.uploader.upload(req.file.path, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//       return res.status(400).json({
+//         success: false,
+//         message: "Error",
+//       });
+//     }
+//     res.status(200).json({
+//       success: true,
+//       message: "Uploaded!",
+//       data: result,
+//     });
+//   });
+// });
 
 module.exports = uploadRouter;
